89509fa0efd91517f27158859506aad6e1edbb8e
Polina Katelkina <p.katelkina@xlab-game.com>
2025-12-12 18:02:25 +0300

    Finish lesson

--- Sample/Assets/Scripts/Magic/Effects/HealEffect.cs
+++ Sample/Assets/Scripts/Magic/Effects/HealEffect.cs
@@ -1,19 +1,19 @@
-using Entities;
 using System;
 using UnityEngine;
+using Entities;
 
 namespace Magic.Effects
 {
     [Serializable]
-    public class AttackEffect : IEffect
+    public sealed class HealEffect : IEffect
     {
-        [SerializeField][Min(0)] private float m_damage;
+        [SerializeField][Min(0)] private float m_heal;
 
         public void Apply(IEffectable effectable)
         {
             if (effectable is IHealth health)
             {
-                health.TakeDamage(m_damage);
+                health.Heal(m_heal);
             }
         }
     }
---
+++ Sample/Assets/Scripts/Magic/Spells/Data/MagicConfig.cs
@@ -0,0 +1,21 @@
+using Magic.Spells.Data;
+using UnityEngine;
+
+namespace Magic.Elements
+{
+    public sealed class MagicConfig : ScriptableObject
+    {
+        [SerializeField] private ElementsData m_elementsData;
+        [SerializeField] private SpellsDatabase m_spellDataBase;
+
+        [SerializeField][Min(1)] private int m_maxElements = 3;
+        [SerializeField][Min(0)] private float m_cancelCooldown = 0.3f;
+
+        public ElementsData elementsData => m_elementsData;
+        public SpellsDatabase spellsDatabase => m_spellDataBase;
+
+        public int maxElements => m_maxElements;
+
+        public float cancelCooldown => m_cancelCooldown;
+    }
+}
