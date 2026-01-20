bbb52f3031fd3bd4a8df2ea1d179d14fd2abc1f4
Polina Katelkina <p.katelkina@xlab-game.com>
2026-01-20 18:02:15 +0300

    Add buffs

---
+++ Sample/Assets/Scripts/Magic/Buffs/Base/BaseBuff.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Magic.Buffs.Base
+{
+    [Serializable]
+    public abstract class BaseBuff : IBuff
+    {
+        [field: SerializeField]
+        public string Id { get; private set; }
+
+        protected BuffContainer container { get; private set; }
+
+        public BaseBuff() { }
+
+        protected BaseBuff(string id)
+        {
+            Id = id;
+        }
+
+        public void Initialize(BuffContainer container)
+        {
+            this.container = container;
+            OnInitialized();
+        }
+
+        protected virtual void OnInitialized() { }
+
+        public void Deinitialize()
+        {
+            OnDeinitializing();
+
+            container.Remove(this);
+            container = null;
+        }
+
+        protected virtual void OnDeinitializing() { }
+
+        public virtual void Update(float deltaTime) { }
+
+        public abstract object Clone();
+    }
+}
